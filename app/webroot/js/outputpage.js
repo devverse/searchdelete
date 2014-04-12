10ec97d33f4a1cfe6f23e29a959b8ff6979b81d4
GeoffChin <chin.geoff@gmail.com>
2014-04-12 16:03:03 -0400

    -g buttons and js to start output of search result

---
+++ app/webroot/js/outputpage.js
@@ -0,0 +1,15 @@
+$(function(){
+	function pdfPage(){
+		var form = $('#nextresult');
+		form.append("<input type='hidden' name='email' value='chin.geoff@gmail.com'/>");
+		form.submit();
+	}
+
+	function emailPage(){
+		var form = $('#nextresult');
+		form.append("<input type='hidden' name='pdf' value='1'/>");
+		form.submit();
+	}
+    $('body').on('click', '.pdf-btn',pdfPage);
+    $('body').on('click', '.email-btn',emailPage);
+});
