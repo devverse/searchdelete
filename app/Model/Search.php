3c945f3cf205dcc99c7922d91f65c84c2986b126
GeoffChin <chin.geoff@gmail.com>
2014-06-27 01:33:00 -0400

    -g bug remove echo

--- app/Model/Search.php
+++ app/Model/Search.php
@@ -360,7 +360,7 @@ class Search extends AppModel {
 		// {
 		// 	$sql = 'SELECT * FROM fullrecords join (' . $sql.')as t On t.id = fullrecords.id';
 		// }
-		echo $sql;
+		
 		$records = $this->query($sql,false);
 		$recordcount = $this->query('SELECT FOUND_ROWS()');
 		$this->recordcount = $recordcount[0][0]["FOUND_ROWS()"];
