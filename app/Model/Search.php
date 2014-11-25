bf6e0459b20d1c0ae165fe41e74a843b7e89dd90
Devverse <Devverse@gmail.com>
2014-11-24 23:34:15 -0500

    Factoring county name

--- app/Model/Search.php
+++ app/Model/Search.php
@@ -4,27 +4,6 @@ class Search extends AppModel {
 	public $useTable = false;
 	public $limit = false;
 	public $validate = array(
-			//network name is blank if not carewisconsin
-			// 'network_name'=>array(
-			// 	'Cannot be blank. Must be Alpha Numeric Character' => array(
-	  //               'rule'     => array('custom', '/^[a-z0-9 ]*$/i'),
-	  //               'allowEmpty' => false,
-   //          	)
-			// ),
-   //          'providertype_name'=>array(
-			// 	'Provider type cannot be blank. Must be Alpha Numeric Character' => array(
-	  //               'rule'     => array('custom', '/^[a-z0-9 ]*$/i'),
-	  //               'allowEmpty' => false
-   //          		)
-			// ),
-			// 'countie_name'=>array(
-			// 	'Please select a County' => array(
-	  //               'rule'=>array('locationOrCountieOrPracticeName'),
-   //          	),
-			// 	'Must be Alpha Numeric Character' => array(
-	  //               'rule'     => array('custom', '/^[a-z0-9 ]*$/i')
-   //          	)
-			// ),
 			'street_address'=>array(
 				'Must be Alpha Numeric Character' => array(
 	                'rule'     => array('custom', '/^[a-z0-9 ]*$/i'),
@@ -120,12 +99,6 @@ class Search extends AppModel {
 	                'allowEmpty' => false,
             	)
             ),
-			// 'specialtie_name'  => array(
-			// 	'Must be Alpha Numeric Character' => array(
-	  //               'rule'     => array('custom', '/^[a-z0-9 ]*$/i'),
-	  //               'allowEmpty' => false,
-   //          	)
-   //          ),
 			'acceptnew'  => array(
 				'Must be Alpha Numeric Character' => array(
 	                'rule'     => array('custom', '/^[a-z0-9 ]*$/i'),
@@ -222,8 +195,6 @@ class Search extends AppModel {
 
 	public function getCoordinatesFromAddress()
 	{
-		//todo set errors and throws
-		//form error
 		$d = $this->data['Search'];
 		$address  = ($d['street_address'] !='')? str_replace(' ', '+', $d['street_address']).'+': '';
 		$address .= ($d['city']!='')? str_replace(' ', '+', $d['city']).'+': '';
@@ -260,48 +231,6 @@ class Search extends AppModel {
 	public function formatProviders($provider_results)
 	{
 		$provider_array = $provider_results;
-		// foreach($provider_results as $provider)
-		// {
-		// 	$curr_prov_id 	= $provider['providers']['id'];
-		// 	$curr_loc_id 	= $provider['locations']['id'];
-		// 	$curr_lang 		= $provider['languages']['name'];
-		// 	$curr_insur 	= $provider['insurances']['name'];
-		// 	$curr_spec 		= $provider['specialties']['name'];
-		// 	$curr_prov 		= $provider['providers'];
-		// 	$curr_loc 		=  $provider['locations'];
-
-		// 	if(!isset($provider_array[$curr_prov_id])) //provider  doesnt exist in list
-		// 	{//SET INITIAL PROVIDER INFO INTO ARRAY WITH ID AS KEY
-		// 		$provider_array[$curr_prov_id] = $curr_prov;
-		// 		$provider_array[$curr_prov_id]['languages'] = array($curr_lang);
-		// 		$provider_array[$curr_prov_id]['insurances'] = array($curr_insur);
-		// 		$provider_array[$curr_prov_id]['specialties'] = array($curr_spec);
-		// 		$provider_array[$curr_prov_id]['locations'] = array($curr_loc);
-		// 		//add locations//addinsurances //add languages
-		// 	}
-		// 	else//provider exist in aray
-		// 	{//check if attributes exist and push if doesnt
-		// 		if(!in_array($curr_lang, $provider_array[$curr_prov_id]['languages']))
-		// 			$provider_array[$curr_prov_id]['languages'][] = $curr_lang;
-		// 		if(!in_array($curr_insur, $provider_array[$curr_prov_id]['insurances']))				
-		// 			$provider_array[$curr_prov_id]['insurances'][] = $curr_insur;
-		// 		if(!in_array($curr_spec, $provider_array[$curr_prov_id]['specialties']))				
-		// 			$provider_array[$curr_prov_id]['specialties'][] = $curr_spec;
-
-		// 		foreach($provider_array[$curr_prov_id]['locations'] as $providers_loc)
-		// 		{
-		// 			$is_in = false;
-		// 			if($providers_loc['id'] == $curr_loc_id)
-		// 			{
-		// 				$is_in = true;
-		// 				break;
-		// 			}
-		// 		}
-		// 		if(!$is_in)
-		// 			$provider_array[$curr_prov_id]['locations'][] = $curr_loc;
-		// 	}
-		// }
-		
 		return $provider_array;
 	}
 
@@ -310,9 +239,9 @@ class Search extends AppModel {
 		$d = $this->data['Search'];
 //SQL_CALC_FOUND_ROWS
 		if($d['practicename'] != ''){
-			$sql = "SELECT SQL_CALC_FOUND_ROWS company , lob, provId,category,specialty,practicename,firstname,middlename,lastname,suffix,degree,address,suite,city,state,zip,zip4,county,servicearea,phone,fax,gender,handicap,acceptingnew,acceptsmedicaid,hospaffiliations,languages,officehours,customfield1desc,customfield1ind,customfield2desc,customfield2ind,customfield3desc,customfield3ind,latitude_str,longitude_str,latitude,longitude,id FROM fullrecords WHERE ";
+			$sql = "SELECT SQL_CALC_FOUND_ROWS company, county, lob, provId,category,specialty,practicename,firstname,middlename,lastname,suffix,degree,address,suite,city,state,zip,zip4,county,servicearea,phone,fax,gender,handicap,acceptingnew,acceptsmedicaid,hospaffiliations,languages,officehours,customfield1desc,customfield1ind,customfield2desc,customfield2ind,customfield3desc,customfield3ind,latitude_str,longitude_str,latitude,longitude,id FROM fullrecords WHERE ";
 		}else{
-			$sql = "SELECT SQL_CALC_FOUND_ROWS company , lob, provId,category,specialty,practicename,firstname,middlename,lastname,suffix,degree,address,suite,city,state,zip,zip4,county,servicearea,phone,fax,gender,handicap,acceptingnew,acceptsmedicaid,hospaffiliations,languages,officehours,customfield1desc,customfield1ind,customfield2desc,customfield2ind,customfield3desc,customfield3ind,latitude_str,longitude_str,latitude,longitude,id FROM fullrecords WHERE ";
+			$sql = "SELECT SQL_CALC_FOUND_ROWS company, county, lob, provId,category,specialty,practicename,firstname,middlename,lastname,suffix,degree,address,suite,city,state,zip,zip4,county,servicearea,phone,fax,gender,handicap,acceptingnew,acceptsmedicaid,hospaffiliations,languages,officehours,customfield1desc,customfield1ind,customfield2desc,customfield2ind,customfield3desc,customfield3ind,latitude_str,longitude_str,latitude,longitude,id FROM fullrecords WHERE ";
 		}
 
 		$distance = ($d['distance_c'] != '')? $d['distance_c'] : $d['distance'];
@@ -333,7 +262,7 @@ class Search extends AppModel {
 		}elseif ($d['countie_name']!='none'){
 	//		$sql .= " (county collate latin1_swedish_ci = '{$d['countie_name']}' OR ";
 	//		$sql .= " servicearea like '%{$d['countie_name']}%') ";
-			$sql .= " servicearea like '%{$d['countie_name']}%' ";
+			$sql .= " county like '%{$d['countie_name']}%' ";
 		}elseif ($d['practicename'] == '' && $d['providertype_name']!='none') {
 			$sql .= ' 1 =1 ';
 		}else{
@@ -372,6 +301,10 @@ class Search extends AppModel {
 	{
 		$d = $this->data['Search'];
 		$sql = '';
+
+		if(isset($d['countie_name']) && strtolower($d['countie_name'])!='none')
+			$sql .= " AND lob collate latin1_swedish_ci ='{$d['countie_name']}'";
+
 		if(isset($d['network_name']) && strtolower($d['network_name'])!='none')
 			$sql .= " AND lob collate latin1_swedish_ci ='{$d['network_name']}'";
 
@@ -407,67 +340,7 @@ class Search extends AppModel {
 
 		if(isset($d['lastname']) && strtolower($d['lastname'])!='none' && strtolower($d['lastname'])!='')
 			$sql .= " AND lastname LIKE '%{$d['lastname']}%'";
-
-		// if(isset($d['practicename']) && strtolower($d['practicename'])!='none' && strtolower($d['practicename'])!='')
-		// 	$sql .= " OR practicename LIKE '%{$d['practicename']}%'";
 		return $sql;
 	}
-
-	// function old()
-	// {
-	// 	$this->query( "set @latitude=".$coor_array['lat'].";",false);
-	// 	$this->query( "set @longitude=".$coor_array['long'].";",false);
-	// 	$this->query( "set @radius=".$distance.";",false);
-	// 	$this->query( "set @lng_min = @longitude - @radius/abs(cos(radians(@latitude))*69.444);",false);
-	// 	$this->query(  "set @lng_max = @longitude + @radius/abs(cos(radians(@latitude))*69.444);",false);
-	// 	$this->query(  "set @lat_min = @latitude - (@radius/69.444);",false);
-	// 	$this->query(  "set @lat_max = @latitude + (@radius/69.444);",false);
-	// 	$this->query( "set SQL_BIG_SELECTS=1;",false);
-
-	// 	$sql = "SELECT providers.* , locations.*, insurances.name, languages.name, specialties.name from providers 
-	// 	JOIN providers_specialties
-	// 	ON providers.id = providers_specialties.provider_id
-	// 	JOIN specialties 
-	// 	ON specialties.id = providers_specialties.specialtie_id
-	// 	";
-	// 	if($filter_array['insurance_id'] != '0')
-	// 		$sql .=" LEFT JOIN providers_insurances 
-	// 		ON providers.id = providers_insurances.provider_id
-	// 		LEFT JOIN insurances 
-	// 		ON insurances.id = providers_insurances.insurance_id ";
-	// 	else
-	// 		$sql .= " JOIN (select 'N/A' as name ) insurances ON 1=1 ";
-
-	// 	$sql .= " JOIN providers_languages 
-	// 	ON providers.id = providers_languages.provider_id
-	// 	JOIN languages 
-	// 	ON languages.id = providers_languages.language_id
-	// 	JOIN providers_locations
-	// 	ON providers.id = providers_locations.provider_id
-	// 	JOIN (
-	// 		SELECT * FROM locations 
-	// 		WHERE (longitude BETWEEN @lng_min AND @lng_max) 
-	// 			AND (latitude BETWEEN @lat_min and @lat_max)) locations 
-	// 	ON providers_locations.location_id = locations.id WHERE 1 = 1 ";
-
-	// 	if(isset($filter_array['specialtie_id']) && $filter_array['specialtie_id'] != '0')
-	// 		$sql .= " AND specialties.id = ".$filter_array['specialtie_id'];
-	// 	if(isset($filter_array['insurance_id']) && $filter_array['insurance_id'] != '0')
-	// 		$sql .= " AND insurances.id = ".$filter_array['insurance_id'];
-	// 	if(isset($filter_array['language_id']) && $filter_array['language_id'] != '0')
-	// 		$sql .= " AND languages.id = ".$filter_array['language_id'];
-	// 	if(isset($filter_array['location_id']) && $filter_array['location_id'] != '0')
-	// 		$sql .= " AND locations.id = ".$filter_array['location_id'];
-	// 	if(isset($filter_array['gender']) && $filter_array['gender'] != '0')
-	// 		$sql .= " AND providers.g = '".$filter_array['gender']."'";
-
-	// 	// if(isset($filter_array['wheelchairaccessible']))
-	// 	// 	$sql .= " AND locations.wheelchair_accessible = 1";
-	// 	// if(isset($filter_array['location_id']) && $filter_array['location_id'] != '0')
-	// 	// 	$sql .= " AND locations.id = ".$filter_array['location_id'];
-	// 	$sql .= ' LIMIT '.$start.' , '.$limit ;
-
-	// }
-
 }
 ?>
