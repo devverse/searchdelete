e3fcca19266da5769e81a24b083d307f3e7f1a7b
Robbins Cleozier <rcleozier@gmail.com>
2016-04-27 00:02:39 -0400

    try catch

--- app/Controller/SearchController.php
+++ app/Controller/SearchController.php
@@ -116,134 +116,139 @@ class SearchController extends AppController {
 
 	public function result($client,$ntwk_ind=false,$client_orig_name)
 	{
-		Configure::write('Model.globalSource', $client['Client']['cake_db_config']);
-		$this->loadModel('Search');
-		$request_data = $this->request->data;
+		try {
+			Configure::write('Model.globalSource', $client['Client']['cake_db_config']);
+			$this->loadModel('Search');
+			$request_data = $this->request->data;
 
-		//start for indexing every 25 results
-		$request_data['start'] = isset($request_data['start']) && $request_data['start']>=0 ? $request_data['start']:0;
-		
-		if(($request_data['street_address'] !=''||$request_data['city'] !='') && $request_data['state'] == 'None' && strpos($client_orig_name, 'carewisconsin') !==false)
-		{
-			$request_data['state'] = 'WI';
-		}
+			//start for indexing every 25 results
+			$request_data['start'] = isset($request_data['start']) && $request_data['start']>=0 ? $request_data['start']:0;
+			
+			if(($request_data['street_address'] !=''||$request_data['city'] !='') && $request_data['state'] == 'None' && strpos($client_orig_name, 'carewisconsin') !==false)
+			{
+				$request_data['state'] = 'WI';
+			}
 
-		$this->Search->set($request_data);
+			$this->Search->set($request_data);
 
-		if($this->Search->validates())
-		{
-			if(isset($request_data['pdf']) || isset($request_data['email']))
-			$this->Search->setLimit(25);
+			if($this->Search->validates())
+			{
+				if(isset($request_data['pdf']) || isset($request_data['email']))
+				$this->Search->setLimit(25);
 
-			$results = $this->Search->getResults();
-			$resultcount = isset($this->Search->recordcount)?$this->Search->recordcount:false;
-		}
-		else
-		{
-			$error = $this->Search->validationErrors;
-			$err_field = key($error);
-			$err_msg = $error[$err_field][0];
-			$this->Session->setFlash($err_msg, 'default', array(), $err_field);
-			//$this->Session->setFlash(array($err_field,$err_msg));
-			//var_dump($this->Search->invalidFields());
-			return $this->redirect(array('action' => $client_orig_name));
-		}
-
-		$param = array(
-			'client_name'=> $client['Client']['name'],
-			'srch_filter'=>$request_data,
-			'results'=>$results['providers'],
-			'resultcount'=>$resultcount,
-			'locations'=>$results['locations'],
-			'coor'=>$results['coor_array'],
-			'title'=>'Search Results for '.ucfirst($client['Client']['name']),
-			'client_url_name'=>$client_orig_name,
-			'req_data'=>$request_data
-		);
-
-		if($ntwk_ind != false)
-			$this->set('asset_folder', $client['Client']['asset_folder_name']."_$ntwk_ind");
-		else
-			$this->set('asset_folder', $client['Client']['asset_folder_name']);
-
-		if(isset($request_data['pdf']) || isset($request_data['email']))
-		{
-			$this->autoRender = false;
-
-			$view = new View($this, false);
-			$view->set($param);
-
-			if(isset($request_data['pdf'])){
-				$static_map_link = $this->_buildStaticMap($param);
-				$view->set('statimgname','sdf');
-				if($static_map_link)
-				{
-					//curl img
-					$s = curl_init();
-					curl_setopt($s, CURLOPT_URL,$static_map_link);
-			        curl_setopt($s, CURLOPT_VERBOSE, 0);
-					curl_setopt($s, CURLOPT_HEADER, 0);
-					curl_setopt($s, CURLOPT_RETURNTRANSFER, 1); 
-					$imgdata = curl_exec($s); 
-					curl_close($s);
-
-					$filename = $this->_randString(10);
-					$path = "../webroot/files/{$filename}.png";
-					file_put_contents($path, $imgdata);
-					$view->set('statimgname',$filename);
-				}
-
-				$view->layout = 'result';
-				$view_output = $view->render($client['Client']['view_prefix_name'].'resultpdf');
-
-				set_time_limit(0);
-				require_once(dirname(__FILE__).'/../Vendor/dompdf/dompdf_config.inc.php');
-
-				$dompdf = new DOMPDF();
-				$dompdf->set_paper('letter');
-				$dompdf->load_html($view_output);
-				$dompdf->render();
-				$dompdf->stream("provider.pdf");
-
-				if($static_map_link)
-					unlink($path);
-				// set_time_limit(0);
-				// require_once(dirname(__FILE__).'/../Vendor/html2pdf_v4.03/html2pdf.class.php');
-			 //    $html2pdf = new HTML2PDF('P','A4','fr');
-			 //    $html2pdf->WriteHTML($view_output);
-			 //    $html2pdf->Output('proivderlist.pdf','D');
+				$results = $this->Search->getResults();
+				$resultcount = isset($this->Search->recordcount)?$this->Search->recordcount:false;
 			}
 			else
 			{
-				//email using postfix
-				$view->set('maplink',$this->_buildStaticMap($param));
-				$view->layout = 'result';
-				$view_output = $view->render($client['Client']['view_prefix_name'].'resultemail');
-
-				$client_email = $request_data['email'];
-				$Email = new CakeEmail();
-				$Email->emailFormat('html');
-				$Email->from(array('noreply@geosearch.commanddirect.com' => ucfirst($client['Client']['name']).' Provider Search Results'));
-				//$Email->from(array('chin.geoff@gmail.com' => 'Provider Search List'));
-				$Email->to($client_email);
-				$Email->subject(ucfirst($client['Client']['name']).' Search Results');
-				$Email->send($view_output);
-				
-				$resp_view = new View($this, false);
-				$resp_view->set($param);
-				$resp_view->set('client_email',$client_email);
-				$resp_view->layout = 'result';
-				echo $resp_view->render($client['Client']['view_prefix_name'].'resultemaillanding');
-			
+				$error = $this->Search->validationErrors;
+				$err_field = key($error);
+				$err_msg = $error[$err_field][0];
+				$this->Session->setFlash($err_msg, 'default', array(), $err_field);
+				//$this->Session->setFlash(array($err_field,$err_msg));
+				//var_dump($this->Search->invalidFields());
+				return $this->redirect(array('action' => $client_orig_name));
 			}
-			
-		}
-		else
-		{
-			$this->autoRender = false;
-			$this->set($param);
-			$this->layout = 'result';
-			$this->render($client['Client']['view_prefix_name'].'result');
+
+			$param = array(
+				'client_name'=> $client['Client']['name'],
+				'srch_filter'=>$request_data,
+				'results'=>$results['providers'],
+				'resultcount'=>$resultcount,
+				'locations'=>$results['locations'],
+				'coor'=>$results['coor_array'],
+				'title'=>'Search Results for '.ucfirst($client['Client']['name']),
+				'client_url_name'=>$client_orig_name,
+				'req_data'=>$request_data
+			);
+
+			if($ntwk_ind != false)
+				$this->set('asset_folder', $client['Client']['asset_folder_name']."_$ntwk_ind");
+			else
+				$this->set('asset_folder', $client['Client']['asset_folder_name']);
+
+			if(isset($request_data['pdf']) || isset($request_data['email']))
+			{
+				$this->autoRender = false;
+
+				$view = new View($this, false);
+				$view->set($param);
+
+				if(isset($request_data['pdf'])){
+					$static_map_link = $this->_buildStaticMap($param);
+					$view->set('statimgname','sdf');
+					if($static_map_link)
+					{
+						//curl img
+						$s = curl_init();
+						curl_setopt($s, CURLOPT_URL,$static_map_link);
+				        curl_setopt($s, CURLOPT_VERBOSE, 0);
+						curl_setopt($s, CURLOPT_HEADER, 0);
+						curl_setopt($s, CURLOPT_RETURNTRANSFER, 1); 
+						$imgdata = curl_exec($s); 
+						curl_close($s);
+
+						$filename = $this->_randString(10);
+						$path = "../webroot/files/{$filename}.png";
+						file_put_contents($path, $imgdata);
+						$view->set('statimgname',$filename);
+					}
+
+					$view->layout = 'result';
+					$view_output = $view->render($client['Client']['view_prefix_name'].'resultpdf');
+
+					set_time_limit(0);
+					require_once(dirname(__FILE__).'/../Vendor/dompdf/dompdf_config.inc.php');
+
+					$dompdf = new DOMPDF();
+					$dompdf->set_paper('letter');
+					$dompdf->load_html($view_output);
+					$dompdf->render();
+					$dompdf->stream("provider.pdf");
+
+					if($static_map_link)
+						unlink($path);
+					// set_time_limit(0);
+					// require_once(dirname(__FILE__).'/../Vendor/html2pdf_v4.03/html2pdf.class.php');
+				 //    $html2pdf = new HTML2PDF('P','A4','fr');
+				 //    $html2pdf->WriteHTML($view_output);
+				 //    $html2pdf->Output('proivderlist.pdf','D');
+				}
+				else
+				{
+					//email using postfix
+					$view->set('maplink',$this->_buildStaticMap($param));
+					$view->layout = 'result';
+					$view_output = $view->render($client['Client']['view_prefix_name'].'resultemail');
+
+					$client_email = $request_data['email'];
+					$Email = new CakeEmail();
+					$Email->emailFormat('html');
+					$Email->from(array('noreply@geosearch.commanddirect.com' => ucfirst($client['Client']['name']).' Provider Search Results'));
+					//$Email->from(array('chin.geoff@gmail.com' => 'Provider Search List'));
+					$Email->to($client_email);
+					$Email->subject(ucfirst($client['Client']['name']).' Search Results');
+					$Email->send($view_output);
+					
+					$resp_view = new View($this, false);
+					$resp_view->set($param);
+					$resp_view->set('client_email',$client_email);
+					$resp_view->layout = 'result';
+					echo $resp_view->render($client['Client']['view_prefix_name'].'resultemaillanding');
+				
+				}
+				
+			}
+			else
+			{
+				$this->autoRender = false;
+				$this->set($param);
+				$this->layout = 'result';
+				$this->render($client['Client']['view_prefix_name'].'result');
+			}
+
+		} catch (Exception $e) {
+		    echo 'Caught exception: ',  $e->getMessage(), "\n";
 		}
 	}
 
