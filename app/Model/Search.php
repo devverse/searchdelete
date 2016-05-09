a29a90ed3e50e1ef740ed5c870e009aa82f26f41
Robbins Cleozier <robbins.cleozier@cmi-robbins.cleozier-2140-mbpr>
2016-05-09 19:11:52 -0400

    Updated search logic

--- app/Model/Search.php
+++ app/Model/Search.php
@@ -338,6 +338,9 @@ class Search extends AppModel {
 		if(isset($d['handicapaccess']) && strtolower($d['handicapaccess'])!='none')
 			$sql .= " AND handicap collate latin1_swedish_ci ='{$d['handicapaccess']}'";
 
+		if(isset($d['zipcode']) && strtolower($d['zipcode'])!='none')
+			$sql .= " AND zip4 LIKE '%{$d['zipcode']}%'"
+
 		if(isset($d['insurance_name']) && strtolower($d['insurance_name'])!='none')
 			$sql .= " AND insurance collate latin1_swedish_ci ='{$d['insurance_name']}'";
 
@@ -346,6 +349,9 @@ class Search extends AppModel {
 
 		if(isset($d['lastname']) && strtolower($d['lastname'])!='none' && strtolower($d['lastname'])!='')
 			$sql .= " AND lastname LIKE '%{$d['lastname']}%'";
+
+		$sql .= " AND ORDER BY practicename ASC";
+
 		return $sql;
 	}
 }
