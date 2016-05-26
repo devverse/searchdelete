5151ca9dd7691260cb255c23219d80b4e1866364
Robbins Cleozier <robbins.cleozier@cmi-robbins.cleozier-2140-mbpr>
2016-05-26 13:05:50 -0400

    readded

--- app/Model/Search.php
+++ app/Model/Search.php
@@ -341,11 +341,11 @@ class Search extends AppModel {
 		if(isset($d['acceptmedicare']) && strtolower($d['acceptmedicare'])!='none')
 			$sql .= " AND acceptsmedicare collate latin1_swedish_ci ='{$d['acceptmedicare']}'";
 
-		// if(isset($d['acceptmedicaid']) && strtolower($d['acceptmedicaid'])!='none')
-		// 	$sql .= " AND acceptsmedicaid collate latin1_swedish_ci ='{$d['acceptmedicaid']}'";
+		if(isset($d['acceptmedicaid']) && strtolower($d['acceptmedicaid'])!='none')
+			$sql .= " AND acceptsmedicaid collate latin1_swedish_ci ='{$d['acceptmedicaid']}'";
 
-		// if(isset($d['handicapaccess']) && strtolower($d['handicapaccess'])!='none')
-		// 	$sql .= " AND handicap collate latin1_swedish_ci ='{$d['handicapaccess']}'";
+		if(isset($d['handicapaccess']) && strtolower($d['handicapaccess'])!='none')
+			$sql .= " AND handicap collate latin1_swedish_ci ='{$d['handicapaccess']}'";
 
 		// if(isset($d['zipcode']))
 		// 	$sql .= " AND zip4 LIKE '%{$d['zipcode']}%'";
