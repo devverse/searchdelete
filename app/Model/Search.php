977e05ccc8563b08b5b44b1c6dece73954800a8f
Robbins Cleozier <robbins.cleozier@cmi-robbins.cleozier-2140-mbpr>
2016-06-14 19:06:22 -0400

    Movied logic

--- app/Model/Search.php
+++ app/Model/Search.php
@@ -315,22 +315,25 @@ class Search extends AppModel {
 		{
 			// $sql .= ' Group By fullrecords.practicename, fullrecords.category,fullrecords.specialty LIMIT '.$d['start'].' , '.$limit ;
 			
-			if(isset($d['search_user']) && strtolower($d['search_user']) == 'debug') {
+			// if(isset($d['search_user']) && strtolower($d['search_user']) == 'debug') {
+			// 	$sql.= ' ORDER BY (POW((longitude-' . $coor_array['long'] . '),2) + POW((latitude-' . $coor_array['lat'] .'),2))';
+			// }
+
+			// if(isset($d['search_user']) && strtolower($d['search_user']) == 'partnerhealthplan' && empty($d['street_address'])) {
+			// 	$sql.= ' ORDER BY fullrecords.practicename ASC,fullrecords.lastname ASC';
+			// }
+		} else {
+			$sql .= ' Group By fullrecords.address, fullrecords.practicename LIMIT '.$d['start'].' , '.$limit;
+		}
+
+		if(isset($d['search_user']) && strtolower($d['search_user']) == 'debug') {
 				$sql.= ' ORDER BY (POW((longitude-' . $coor_array['long'] . '),2) + POW((latitude-' . $coor_array['lat'] .'),2))';
 			}
 
 			if(isset($d['search_user']) && strtolower($d['search_user']) == 'partnerhealthplan' && empty($d['street_address'])) {
 				$sql.= ' ORDER BY fullrecords.practicename ASC,fullrecords.lastname ASC';
 			}
-		} else {
-			$sql .= ' Group By fullrecords.address, fullrecords.practicename LIMIT '.$d['start'].' , '.$limit;
-		}
 
-		// if(true)
-		// {
-		// 	$sql = 'SELECT * FROM fullrecords join (' . $sql.')as t On t.id = fullrecords.id';
-		// }
-		
 		$records = $this->query($sql,false);
 		$recordcount = $this->query('SELECT FOUND_ROWS()');
 		$this->recordcount = $recordcount[0][0]["FOUND_ROWS()"];
