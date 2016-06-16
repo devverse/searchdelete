edfb3230cd11c03cea2a6343d9258dcb92fbe281
Robbins Cleozier <rcleozier@gmail.com>
2016-06-16 00:19:21 -0400

    Updated limit

--- app/Model/Search.php
+++ app/Model/Search.php
@@ -318,7 +318,7 @@ class Search extends AppModel {
 		} else {
 
 			if(isset($d['search_user']) && strtolower($d['search_user']) == 'partnerhealthplan' && $coor_array && $coor_array['lat'] && $coor_array['long']) {
-				$sql.= ' ORDER BY (POW((longitude-' . $coor_array['long'] . '),2) + POW((latitude-' . $coor_array['lat'] .'),2))' . ' LIMIT '.$d['start'].' , '.$limit;;
+				$sql.= ' ORDER BY (POW((longitude-' . $coor_array['long'] . '),2) + POW((latitude-' . $coor_array['lat'] .'),2))';
 			}
 
 			// if(isset($d['search_user']) && strtolower($d['search_user']) == 'partnerhealthplan' && empty($d['street_address'])) {
