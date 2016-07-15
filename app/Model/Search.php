737e8770e01f67d945d66ce6a03c0a4c7244c635
Robbins Cleozier <robbins.cleozier@cmi-robbins.cleozier-2140-mbpr>
2016-07-15 13:36:45 -0400

    test

--- app/Model/Search.php
+++ app/Model/Search.php
@@ -362,13 +362,13 @@ class Search extends AppModel {
 		// 	$sql .= " AND lob collate latin1_swedish_ci ='{$d['network_name']}'";
 
 		if(isset($d['specialtie_name']) && strtolower($d['specialtie_name'])!='none')
-			$sql .= " (AND specialty ='{$d['specialtie_name']}') ";
+			$sql .= " AND specialty ='{$d['specialtie_name']}'";
 
 		if(isset($d['providertype_name']) && strtolower($d['providertype_name'])!='none')
 			$sql .= " AND category  ='{$d['providertype_name']}'";
 
 		if(isset($d['language_name']) && strtolower($d['language_name'])!='none')
-			$sql .= " (AND languages LIKE'%{$d['language_name']}%') ";
+			$sql .= " AND languages LIKE'%{$d['language_name']}%'";
 
 		if(isset($d['acceptnew']) && strtolower($d['acceptnew'])!='none')
 			$sql .= " AND acceptingnew collate latin1_swedish_ci ='{$d['acceptnew']}'";
